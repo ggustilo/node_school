a1ce2928ace86e3138b906f12475193712a8c0d8
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-04-07 16:34:15 -0500

    Add exercises

--- functionalJS/reduce.js
+++ functionalJS/reduce.js
@@ -1,12 +1,10 @@
 function countWords(inputWords) {
-	var numTimes = {};
-	inputWords.reduce(function(previous, current){
-		if(numTimes[current]) {
-			return numTimes[current] += 1;
+	inputWords.reduce(function(map, current){
+		if(map[current]) {
+			return map[current] += 1;
 		}
-		numTimes[current] = 1;
+		map[current] = 1;
 	}, {});
-	return numTimes;
 }
 
 module.exports = countWords;
