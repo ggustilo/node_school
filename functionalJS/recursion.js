5b3dc1e48c24f36298eb121a9f70efc6fdd30706
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-04-12 10:09:41 -0500

    Mostly working recursion - need to filter for unique values

--- functionalJS/recursion.js
+++ functionalJS/recursion.js
@@ -1,26 +1,34 @@
 //Pseudocode:
 
-// iterate through the current object
-//		if the current object has a property called dependencies
-//			call the method again
-//		else
-//			iterate through it and add each element plus @ plus element["version"] to an array
-//		end
-// return that array
+// iterate through the keys of the tree
+//		if a key is 'dependencies'
+//			iterate through the dependencies
+//			if a key is version
+//				grab its parent key
+//				add parent plus @ plus child to dependencies array
+//				return
 
 function getDependencies(tree, dependencies) {
 	var allDependencies = dependencies || [];
 	console.log(tree);
-	var forEach = Array.prototype.forEach;
-
-	forEach.call(tree, function(property){
-		allDependencies.push(property + "@");
-		if (property === "dependencies") {
-			getDependencies(property, allDependencies);
+	
+	Object.keys(tree).forEach(function(key) {
+		console.log(key);
+		if (tree[key].hasOwnProperty("version")) {
+			console.log("Got version");
+			allDependencies.push(key + '@' + tree[key]['version']);
+		}
+		if (tree[key].hasOwnProperty("dependencies")) {
+			console.log("Got sub dependencies");
+			allDependencies.concat(getDependencies(tree[key]['dependencies'], allDependencies));
+		}
+		if (key === "dependencies") {
+			console.log("Got dependencies");
+			allDependencies.concat(getDependencies(tree[key], allDependencies));
 		}
 	});
-
-	return allDependencies;
+	console.log(allDependencies);
+	return allDependencies.sort();
 }
 
 module.exports = getDependencies;
