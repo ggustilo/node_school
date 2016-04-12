5b9ef1b437c2991501962a6785801dbd55064561
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-04-12 12:16:40 -0500

    Currying not quite working

--- functionalJS/currying.js
+++ functionalJS/currying.js
@@ -1,12 +1,37 @@
 
+// function curry3(fun){
+//       return function(one){
+//         return function(two){
+//           return function (three){
+//             return fun(one, two, three)
+//           }
+//         }
+//       }
+//     }
 
 
-function curryN(fn, n) {
-	var levels = n || fn.length;
-	if (levels >= 0) {
-		return fn();
+function curryN(fn, n, args) {
+	var num;
+	var args = args || [];
+	
+	if (typeof n === 'undefined') {
+		num = fn.length;
 	}
-	curryN(fn, --levels);
+	else {
+		num = n;
+	}
+
+	console.log(num);
+	console.log(args);
+	console.log("++++++++++++++++++++");
+
+	if (num >= 0) {
+		args.push(num);
+		return function(num) {
+			return curryN(fn, num - 1, args);
+		}
+	}
+	return fn.apply(null, args);	
 }
 
 module.exports = curryN;
