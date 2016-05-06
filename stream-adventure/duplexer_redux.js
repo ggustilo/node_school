0d08362b2ad98aa9e6a8f762e18e1c690765fb3b
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-05-06 15:20:41 -0500

    Add duplexer redux

--- stream-adventure/duplexer_redux.js
+++ stream-adventure/duplexer_redux.js
@@ -1,16 +1,26 @@
 var duplexer = require('duplexer2');
 var stream = require('stream');
+var through = require('through2').obj;
 var countries = {};
 
-
-module.exports = function(counter) {
-	var writable = new stream.Writable({objectMode:true});
-
-	return duplexer(writable, counter);
+var incrementCountries = function(obj) {
+	var occurrence = obj["country"];
+	countries[occurrence] = (countries[occurrence] || 0) + 1;
 };
 
+module.exports = function(counter) {
+	var write = function(chunk, _, next) {
+		incrementCountries(chunk);
+		next();
+	};
 
+	var end = function() {
+		console.log(countries);
+		counter.setCounts(countries);
+	};
 
-counter.on('end', () => {
-	counter.setCounts(countries);
-});
+	var writable = through(write, end);
+	var duplex = duplexer({objectMode:true}, writable, counter);
+	return duplex;
+};
+
