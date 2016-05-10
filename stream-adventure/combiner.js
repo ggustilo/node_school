d513e03df69498f661c88f7797e0a27b779460b7
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-05-10 14:29:33 -0500

    Add combiner.js

--- stream-adventure/combiner.js
+++ stream-adventure/combiner.js
@@ -1,41 +1,36 @@
 var combine = require('stream-combiner');
 var split = require('split');
-var stream = require('stream');
-var through = require('through2').obj;
+var through = require('through2');
 var zlib = require('zlib');
 
-// Example input: (newline separated)
-//
-// {"type":"genre","name":"cyberpunk"}
-// {"type":"book","name":"Neuromancer"}
-// {"type":"book","name":"Snow Crash"}
-// {"type":"genre","name":"space opera"}
-// {"type":"book","name":"A Deepness in the Sky"}
-// {"type":"book","name":"Void"}
-
-// Example output:
-//
-// {"name":"cyberpunk","books":["Neuromancer","Snow Crash"]}
-// {"name":"space opera","books":["A Deepness in the Sky","Void"]}
 
 module.exports = function() {
+	var newGenre;
+
 	var write = function(chunk, _, next) {
-		this.push(chunk);
+		var obj = JSON.parse(chunk);
+		if (obj.type === "genre") {
+			if (newGenre) {
+				this.push(JSON.stringify(newGenre) + "\n");
+			}
+			newGenre = { name: obj.name, books: []};
+		}
+		else if (obj.type === "book") {
+			newGenre.books.push(obj.name);
+		}
 		next();
 	};
 
-	var end = function(done) {
-		done();
+	var end = function(next) {
+		if (newGenre) {
+			this.push(JSON.stringify(newGenre) + "\n");
+		}
+		next();
 	};
 
-	var read = zlib.createGZip();
-
-	var writable =  through(write, end);
-	var readable = stream.Readable
+	var genrefy = through(write, end);
 	return combine(
-		writable,
-		
-		// group books into genres,
-		// then gzip the output
-	)
-}
+		genrefy,
+		zlib.createGzip()
+	);
+};
