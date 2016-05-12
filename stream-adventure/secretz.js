72a39d7423b521410a50050f0747c85c377436f7
Gabrielle Gustilo <ggustilo@conversantmedia.com>
2016-05-12 16:56:05 -0500

    Made marginal progress

--- stream-adventure/secretz.js
+++ stream-adventure/secretz.js
@@ -4,16 +4,27 @@ var tar = require('tar');
 var parser = tar.Parse();
 var combine = require('stream-combiner');
 var decipher = crypto.createDecipher(process.argv[2], process.argv[3]);
+var hasher = crypto.createHash('md5', { encoding: 'hex' });
+
+// get stuff on stdin
+// unzip it
+// parse it
+// decrypt it
+// get the file contents
+// get the file name
+// hash the file contents
+// output the file contents
+// output a space and the filename
+// output a newline
 
 parser.on('entry', function(e) {
-	this.push(e + ' ' + e.path + '\n');
+	var hashedContent = e.pipe(hasher);
+	var filename = e.path;
+	console.log(hashedContent + ' ' + filename + '\n');
 });
 
-combine(
-	process.stdin,
-	zlib.createGunzip(),
-	decipher,
-	parser,
-	crypto.createHash('md5', { encoding: 'hex'}),
-	process.stdout
-)
+process.stdin.pipe(zlib.createGunzip())
+			.pipe(decipher)
+			.pipe(parser)
+			.pipe(process.stdout);
+
